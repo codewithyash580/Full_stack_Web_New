435fd4d4442d9ff650a374cef22c1f165a64873b
yashchauhan2025 <chauhanyash098234@gmail.com>
2026-08-03 17:43:00 +0530

    Temporarily remove ThreeHero

--- client-app/src/pages/Experience.jsx
+++ client-app/src/pages/Experience.jsx
@@ -2,7 +2,7 @@ import { useLayoutEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-import ThreeHero from "../Components/ThreeHero";
+
 
 import "./Experience.css";
 
@@ -217,7 +217,7 @@ function Experience() {
     >
       
 
-      <ThreeHero />
+      
 
       
 
